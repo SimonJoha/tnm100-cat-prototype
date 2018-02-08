1a04362615794054b126b87f07e402d9e8e88fc3
Jacob Andersson <jacob.m.andersson@gmail.com>
2018-02-08 11:33:07 +0100

    Tabs added

---
+++ src/components/Tabs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tabs">
+    <md-tabs md-alignment="fixed">
+      <md-tab id="tab-home" md-label="Home" md-icon="home"></md-tab>
+      <md-tab id="tab-pages" md-label="Pages" md-icon="pages"></md-tab>
+      <md-tab id="tab-favorites" md-label="Favorites" md-icon="favorite"></md-tab>
+    </md-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabs'
+}
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+  width: 100vw;
+}
+</style>
